bb02743d584b9e9392dc42a3f7d1c342c9f8a84b
Jacob Bednarz <jacob.bednarz@gmail.com>
2020-12-08 13:31:28 +1100

    codify mac config

--- darwin.nix
+++ darwin.nix
@@ -8,4 +8,45 @@
     description = "Jacob Bednarz";
     shell = pkgs.fish;
   };
+
+  # auto-correct
+  system.defaults.NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+
+  # show all file extensions
+  system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
+
+  # always show scrollbars
+  system.defaults.NSGlobalDomain.AppleShowScrollBars = "Always";
+
+  # metric system ftw
+  system.defaults.NSGlobalDomain.AppleMeasurementUnits = "Centimeters";
+  system.defaults.NSGlobalDomain.AppleMetricUnits = 1;
+  system.defaults.NSGlobalDomain.AppleTemperatureUnit = "Celsius";
+
+  # dock
+  system.defaults.dock.autohide = true;
+  system.defaults.dock.orientation = "left";
+  system.defaults.dock.showhidden = true;
+  system.defaults.dock.mru-spaces = false;
+  system.defaults.dock.tilesize = 32;
+
+  # finder
+  system.defaults.finder.AppleShowAllExtensions = true;
+  system.defaults.finder.QuitMenuItem = true;
+  system.defaults.finder.FXEnableExtensionChangeWarning = false;
+
+  # keyboard / trackpad
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToControl = true;
+  system.defaults.trackpad.TrackpadRightClick = true;
+
+  # software updates
+  system.defaults.SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+
+  # login
+  system.defaults.loginwindow.GuestEnabled = false;
 }
