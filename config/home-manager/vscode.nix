f87f8cbc1240b71df32ad9d2971b9ad87a926b40
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-01-05 11:51:14 +1100

    share config for GA and insiders build

--- config/home-manager/vscode.nix
+++ config/home-manager/vscode.nix
@@ -1,3 +1,7 @@
 {
+  # ga
   home.file."Library/Application Support/Code/User/settings.json".source = ../vscode/settings.json;
+
+  # insiders
+  home.file."Library/Application Support/Code - Insiders/User/settings.json".source = ../vscode/settings.json;
 }
