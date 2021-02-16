e9ad038b96b266684df6723a8e4b3cbc10dde5aa
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-02-17 07:47:36 +1100

    more vscode config

--- config/home-manager/vscode.nix
+++ config/home-manager/vscode.nix
@@ -4,4 +4,7 @@
 
   # insiders
   home.file."Library/Application Support/Code - Insiders/User/settings.json".source = ../vscode/settings.json;
+
+  # snippets
+  home.file."Library/Application Support/Code - Insiders/User/snippets/go.json".source = ../vscode/snippets/go.json;
 }
