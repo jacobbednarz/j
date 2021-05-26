5dee90572dbd9e4430fe16a38557916ddcc9d3d6
Jacob Bednarz <jacob.bednarz@gmail.com>
2021-05-26 13:29:35 +1000

    chuck sbin on the path for use

--- config/home-manager/env.nix
+++ config/home-manager/env.nix
@@ -1,7 +1,7 @@
 {
   home.sessionVariables = {
     EDITOR            = "vim";
-    PATH              = "/Users/jacob/.nix-profile/bin:/run/current-system/sw/bin:/Users/jacob/.asdf/shims:/Users/jacob/bin:/Users/jacob/.go/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin/";
+    PATH              = "/Users/jacob/.nix-profile/bin:/run/current-system/sw/bin:/Users/jacob/.asdf/shims:/Users/jacob/bin:/Users/jacob/.go/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin/";
     NIX_PATH          = "darwin-config=/Users/jacob/.config/nixpkgs/darwin.nix:/Users/jacob/.nix-defexpr/channels";
     NIX_SSL_CERT_FILE = "/nix/var/nix/profiles/per-user/jacob/profile/etc/ssl/certs/ca-bundle.crt";
   };
