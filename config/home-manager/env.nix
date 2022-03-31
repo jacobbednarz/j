425e8c84c1a4b723a37dd775a78d9f59669327b9
Jacob Bednarz <jacob.bednarz@gmail.com>
2022-04-01 10:37:14 +1100

    extend path usage

--- config/home-manager/env.nix
+++ config/home-manager/env.nix
@@ -1,8 +1,25 @@
+{ config, pkgs, lib, ... }:
+
 {
   home.sessionVariables = {
     EDITOR            = "vim";
-    PATH              = "/Users/jacob/.nix-profile/bin:/run/current-system/sw/bin:/Users/jacob/.asdf/shims:/Users/jacob/bin:/Users/jacob/.go/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin/";
     NIX_PATH          = "darwin-config=/Users/jacob/.config/nixpkgs/darwin.nix:/Users/jacob/.nix-defexpr/channels";
     NIX_SSL_CERT_FILE = "/nix/var/nix/profiles/per-user/jacob/profile/etc/ssl/certs/ca-bundle.crt";
+
+    PATH = lib.strings.concatStringsSep ":" [
+      "/Users/jacob/.nix-profile/bin"
+      "/run/current-system/sw/bin"
+      "/Users/jacob/.asdf/shims"
+      "/Users/jacob/bin"
+      "/Users/jacob/.go/bin"
+      "/Users/jacob/.cargo/bin"
+      "/Users/jacob/Library/Python/3.9/bin"
+      "/usr/local/bin"
+      "/usr/local/sbin"
+      "/usr/bin"
+      "/bin"
+      "/usr/sbin"
+      "/sbin/"
+    ];
   };
 }
