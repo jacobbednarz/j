ef33646ed8db4fb49e0fcf3313edbc4d2cf8c676
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-08-30 14:20:14 +1000

    add cloudflare warp certs
    Signed-off-by: Jacob Bednarz <jacob.bednarz@gmail.com>

---
+++ config/home-manager/cloudflare-warp.nix
@@ -0,0 +1,4 @@
+{
+  home.file.".local/share/warp/cloudflare.crt".source = ../cloudflare-warp/Cloudflare_CA.crt;
+  home.file.".local/share/warp/cloudflare.pem".source = ../cloudflare-warp/Cloudflare_CA.pem;
+}
