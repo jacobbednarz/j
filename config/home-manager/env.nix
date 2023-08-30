dc0816285b762748617efc5c5fc34beaab08d00b
Jacob Bednarz <jacob.bednarz@gmail.com>
2023-08-30 14:23:25 +1000

    update env
    Signed-off-by: Jacob Bednarz <jacob.bednarz@gmail.com>

--- config/home-manager/env.nix
+++ config/home-manager/env.nix
@@ -3,10 +3,13 @@
 {
   home.sessionVariables = {
     EDITOR            = "vim";
-    NIX_PATH          = "darwin-config=/Users/jacob/.config/nixpkgs/darwin.nix:/Users/jacob/.nix-defexpr/channels";
-    NIX_SSL_CERT_FILE = "/nix/var/nix/profiles/per-user/jacob/profile/etc/ssl/certs/ca-bundle.crt";
+    # NIX_PATH          = "darwin-config=/Users/jacob/.config/home-manager/darwin.nix:/Users/jacob/.nix-defexpr/channels";
+    # NIX_SSL_CERT_FILE = "/nix/var/nix/profiles/per-user/jacob/profile/etc/ssl/certs/ca-bundle.crt";
+    # NIX_SSL_CERT_FILE = "/Users/jacob/.local/share/warp/cloudflare.crt";
+
 
     PATH = lib.strings.concatStringsSep ":" [
+      "/nix/var/nix/profiles/default/bin"
       "/Users/jacob/.asdf/shims"
       "/Users/jacob/.nix-profile/bin"
       "/run/current-system/sw/bin"
